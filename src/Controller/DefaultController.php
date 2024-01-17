c3ec16a02e617fcdb8642f5b7da66fa30e9d276b
Ramiro Monllor <ramiromonllor@gmail.com>
2024-01-16 22:04:04 -0300

    actos

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -5,17 +5,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends AbstractController
 {
     /**
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function index()
+    public function index(): Response
     {
         // return $this->render('index.html.twig', []);
         // return $this->render('fileUpload.html.twig', []);
         // return $this->render('fileSigning.html.twig', []);
-        return $this->render('moduloCaja2.html.twig', []);
+        // return $this->render('moduloCaja1.html.twig', []);
+        // return $this->render('moduloCaja2.html.twig', []);
+
+        $actos = [
+            ['id' => 1, 'nombre' => 'Acto 1', 'registro' => 'Registro 1', 'valorización' => 100],
+        ];
+        return $this->render('mantenedorActos1.html.twig', [
+            'actos' => $actos,
+        ]);
     }
 }
